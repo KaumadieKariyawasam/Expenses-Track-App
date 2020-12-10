13ae752c6c02ccd73c3fbde695e3a637a68d93a5
KAW <kkawmadie@gmail.com>
2020-12-10 13:58:36 +0530

    create flowchartController

--- src/main/java/com/example/vue/repository/FlowchartRepository.java
+++ src/main/java/com/example/vue/repository/FlowchartRepository.java
@@ -5,5 +5,5 @@ import com.example.vue.demo.entities.Flowchart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlowchartRepository extends JpaRepository<Flowchart,Long>{
-    
+    Flowchart findByName(String name);
 }
