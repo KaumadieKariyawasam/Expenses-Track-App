57e030769ed3e03cf74d18e241fa53578741717a
kaumadi <kaumadie@tsdclanka.com>
2021-01-18 15:06:56 +0530

    create kpi repository

---
+++ src/main/java/com/example/vue/demo/repository/KpiRepository.java
@@ -0,0 +1,9 @@
+package com.example.vue.demo.repository;
+
+import com.example.vue.demo.entities.Kpi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KpiRepository extends JpaRepository<Kpi,Long>{
+    
+}
